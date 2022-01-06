bef80a58550dd243878c4e95444bd50c00c4e209
guixou <guillaumejuju@live.fr>
2022-01-06 15:35:56 +0100

    06/01
    New font, rework header, site 3

--- public/js/header.js
+++ public/js/header.js
@@ -2,15 +2,49 @@
 
 document.addEventListener('DOMContentLoaded', function () {
 
+    const navA = document.querySelectorAll("nav a");
+
     window.addEventListener('scroll', function(e){
         
-        const header = document.querySelector(".nav");
+        const nav = document.querySelector(".nav");
+        const header = document.querySelector("header");
+        const button = document.querySelector(".fa-bars");
 
-        if(window.scrollY > 150){
-            header.setAttribute("id", "fixed-header");
+        if(window.scrollY > 80){
+            header.setAttribute("id", "scroll-header");
+            nav.setAttribute("id", "scroll-nav");
+            button.removeAttribute("id", "navButton");
+
         } else {
-            header.removeAttribute("id", "fixed-header");
+            nav.removeAttribute("id", "scroll-nav");
+            header.removeAttribute("id", "scroll-header");
+            button.setAttribute("id", "navButton");
+            
+            for (let i = 0; i < navA.length; i++) {
+                navA[i].removeAttribute("id", "navButton"); 
+            };
         }
     });
+
+    let show = true;
+
+    document.querySelector(".fa-bars").addEventListener('click', function(z){
+        
+        
+        if (show == true){
+            
+            for (let i = 0; i < navA.length; i++) {
+                navA[i].setAttribute("id", "navButton");  
+            };
+            show = false;
+            console.log(show);
+        } else {
+            for (let i = 0; i < navA.length; i++) {
+                navA[i].removeAttribute("id", "navButton"); 
+            };
+            show = true;
+        }
+    })
 });
         
+
